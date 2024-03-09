81535271e3e9c127b45dcf74390962877eaff3a0
CuriousNom <aryanbiswas7585@gmail.com>
2024-03-10 01:43:52 +0530

    gta4l: Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,20 @@
+# Cloning vendor repositories
+git clone --depth=1 https://github.com/CuriousNom/android_vendor_samsung_gta4l.git vendor/samsung/gta4l
+
+# Cloning necessary repository
+git clone --depth=1 https://github.com/Matrixx-Devices/android_hardware_samsung hardware/tmp
+cp -r hardware/tmp/* hardware/samsung/
+rm -rf hardware/tmp
+
+# Cloning kernel repositories
+git clone --depth=1 https://github.com/Matrixx-Devices/android_kernel_samsung_gta4l kernel/samsung/sm6115
+
+# Update KSU Targets
+cd kernel/samsung/sm6115
+rm -rf KernelSU
+git submodule sync
+git submodule update --init --recursive
+cd KernelSU
+git checkout main
+git pull
+cd ../../../..
