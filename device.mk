0eff34fd8b47629af6d549b2a471b91c60090f21
CuriousNom <aryanbiswas7585@gmail.com>
2024-03-10 15:28:24 +0530

    gta4l: Add missing seperator in overlays

--- device.mk
+++ device.mk
@@ -23,7 +23,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Local overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
 # Shipping API level
